09f84858a2d749d6dd4bf613e21e69eea7d27e03
Martin <martinlaiz1710@gmail.com>
2016-04-19 19:17:40 +0200

    Origen

---
+++ CAD/comentarioCAD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.CAD
+{
+    public class comentarioCAD
+    {
+        private string conexion;
+
+
+    }
+}
---
+++ CAD/usuarioCAD.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.CAD
+{
+    public class usuarioCAD
+    {
+        //
+        private string conexion;
+
+        public usuarioCAD(){
+            //Adquiere la cadena de conexión desde un único sitio
+        }
+
+        public bool Crear(usuarioEN usuario)
+        {
+            bool ok = false;
+            //crear usuario
+
+            return ok;
+        }
+
+        public bool Actualizar(usuarioEN usuario)
+        {
+            bool ok = false;
+            //crear usuario
+
+            return ok;
+        }
+
+        public bool Borrar(usuarioEN usuario)
+        {
+            bool ok = false;
+            //crear usuario
+
+            return ok;
+        }
+
+        public usuarioEN dameUsuario(string correo)
+        {
+            usuarioEN usuario = new usuarioEN();
+            //crear usuario
+
+            return usuario;
+        }
+
+
+
+    }
+}
