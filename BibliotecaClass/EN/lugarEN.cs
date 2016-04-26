d6e8c85e0b8578329768f394de8b6c8b4533464d
Martin <martinlaiz1710@gmail.com>
2016-04-26 16:52:37 +0200

    Renovación Proyecto

---
+++ BibliotecaClass/EN/lugarEN.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AplicacionWeb
+{
+    public class lugarEN
+    {
+        private string nombre;
+        private string direccion; 
+        private string descripcion;
+        private string foto_lugar;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Descripcion
+        {
+            get { return descripcion; }
+            set { descripcion = value; }
+        }
+
+        public string Foto_lugar
+        {
+            get { return foto_lugar; }
+            set { foto_lugar = value; }
+        }
+
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+    }
+
+     
+
+}
