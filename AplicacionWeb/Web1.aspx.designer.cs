2202e4cd6e59c1eb74a2a40a5611feb8d515db54
Martin <martinlaiz1710@gmail.com>
2016-05-02 20:36:21 +0200

    nada

--- AplicacionWeb/Web1.aspx.designer.cs
+++ AplicacionWeb/Web1.aspx.designer.cs
@@ -11,23 +11,5 @@ namespace AplicacionWeb {
     
     
     public partial class Web1 {
-        
-        /// <summary>
-        /// Control form1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
-        /// Control Image1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image Image1;
     }
 }
